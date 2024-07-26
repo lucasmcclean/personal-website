5f858627126981fdbbd4770d1a49a7c8da86ba22
Lucas McClean <lucksdeveloper@gmail.com>
2024-07-26 08:34:04 +0200

    feat: header, document component for composition

--- main.go
+++ main.go
@@ -2,17 +2,38 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 
 	"github.com/a-h/templ"
+	"github.com/ljmcclean/mcclean.xyz/reload"
 	"github.com/ljmcclean/mcclean.xyz/views"
 )
 
 const port = ":1234"
+const dev = true
 
 func main() {
-	http.Handle("/", templ.Handler(views.Index("World")))
-	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+	mux := http.NewServeMux()
+
+	mux.Handle("/", templ.Handler(views.Index("Eli")))
+	mux.Handle("/palette", templ.Handler(views.Palette()))
+	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+
+	srv := &http.Server{
+		Handler: mux,
+	}
+
+	if dev {
+		mux.Handle("/reload", http.HandlerFunc(reload.Handler))
+		srv.Handler = reload.BrowserReloadMiddleware(mux)
+	}
+
 	fmt.Println("Listening on port ", port)
-	http.ListenAndServe(port, nil)
+	ln, err := net.Listen("tcp", port)
+	if err != nil {
+		panic(err)
+	}
+
+	srv.Serve(ln)
 }
