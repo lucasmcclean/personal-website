85b8b9be03417b4b92bc1f90b2b663b6c8261beb
Lucas McClean <lucksdeveloper@gmail.com>
2024-08-02 07:55:09 +0200

    fix: move 'mostRecent' into correct closure

--- assets/index.js
+++ assets/index.js
@@ -1,17 +1,44 @@
 "use strict";
 
-const tabs = Array.from(document.getElementsByClassName("tab"));
-const links = Array.from(document.getElementsByClassName("link"));
-let mostRecent = null;
-
 window.onload = function() {
+  const tabs = Array.from(document.getElementsByClassName("tab"));
+  let mostRecent = "";
+
   tabs.forEach((tab) => {
+    tab.addEventListener("click", activateSub);
+    tab.addEventListener("mouseover", activateSub);
+
+    // If the description is already active don't send a network request
     htmx.on("#" + tab.id, "htmx:beforeRequest", (e) => {
-      if (e.srcElement == mostRecent) {
+      let id = e.srcElement.id;
+      console.log(mostRecent, id);
+      if (id == mostRecent) {
         e.preventDefault();
+      } else {
+        mostRecent = id;
       }
-      mostRecent = e.srcElement;
     });
   });
+
+  // Initialize page
+  activateSub({ srcElement: { id: "tl" } })
 }
+
+let previous = null;
+
+function activateSub(e) {
+  let tabName = e.srcElement.id;
+  if (tabName != previous) {
+    deactivateSub(previous);
+    document.getElementById(tabName + "-sub").style.animation = ".3s ease both select-link";
+    previous = tabName;
+  }
+}
+
+function deactivateSub(tabName) {
+  if (tabName != null) {
+    document.getElementById(tabName + "-sub").style.animation = "";
+  }
+}
+
 
